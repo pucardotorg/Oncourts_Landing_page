9d45ee1e4e5b25a5dc9bdb5bd55fb2c6301a70ac
Prati28 <sankhepratiksha3@gmail.com>
2024-11-18 09:56:50 +0530

    changes in announcement section
    Signed-off-by: Prati28 <sankhepratiksha3@gmail.com>

--- data/announcements.js
+++ data/announcements.js
@@ -1,24 +1,25 @@
+// date format should be mm-dd-yyyy
 export const announcementData = [
   {
     id: 1,
-    date: "01-11-2024",
+    date: "11-01-2024",
     heading: "Announcement 1",
     description: "Defects - Physical copy of e-filed cases (14/11/2024)",
-    tag: "General",
+    type: "General",
   },
   {
     id: 2,
-    date: "09-11-2024",
+    date: "11-09-2024",
     heading: "Announcement 2",
     description: "Defects in machine scrutinized files",
-    tag: "General",
+    type: "General",
   },
   {
     id: 3,
-    date: "13-11-2024",
+    date: "11-13-2024",
     heading: "Announcement 3",
     description:
       "Notice - Obituary reference on Thursday, the 14th of November 2024, to condole the sad demise of Advocates: Antony K.J., Ali N.A., Vijayan M.P. and Leela S.",
-    tag: "Obituary",
+    type: "Obituary",
   },
 ];
