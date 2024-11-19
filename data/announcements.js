fc687fbeb6e364d719fdd8b0256d0ed24269dd61
Prati28 <sankhepratiksha3@gmail.com>
2024-11-19 22:47:49 +0530

    changes
    Signed-off-by: Prati28 <sankhepratiksha3@gmail.com>

--- data/announcements.js
+++ data/announcements.js
@@ -1,25 +1,25 @@
 // date format should be mm-dd-yyyy
 export const announcementData = [
-  {
-    id: 1,
-    date: "11-01-2024",
-    heading: "Announcement 1",
-    description: "Defects - Physical copy of e-filed cases (14/11/2024)",
-    type: "General",
-  },
-  {
-    id: 2,
-    date: "11-09-2024",
-    heading: "Announcement 2",
-    description: "Defects in machine scrutinized files",
-    type: "General",
-  },
-  {
-    id: 3,
-    date: "11-13-2024",
-    heading: "Announcement 3",
-    description:
-      "Notice - Obituary reference on Thursday, the 14th of November 2024, to condole the sad demise of Advocates: Antony K.J., Ali N.A., Vijayan M.P. and Leela S.",
-    type: "Obituary",
-  },
+  // {
+  //   id: 1,
+  //   date: "11-01-2024",
+  //   heading: "Announcement 1",
+  //   description: "Defects - Physical copy of e-filed cases (14/11/2024)",
+  //   type: "General",
+  // },
+  // {
+  //   id: 2,
+  //   date: "11-09-2024",
+  //   heading: "Announcement 2",
+  //   description: "Defects in machine scrutinized files",
+  //   type: "General",
+  // },
+  // {
+  //   id: 3,
+  //   date: "11-13-2024",
+  //   heading: "Announcement 3",
+  //   description:
+  //     "Notice - Obituary reference on Thursday, the 14th of November 2024, to condole the sad demise of Advocates: Antony K.J., Ali N.A., Vijayan M.P. and Leela S.",
+  //   type: "Obituary",
+  // },
 ];
