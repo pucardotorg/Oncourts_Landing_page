e2279d27ccd1d82079b598428db533f0444396da
Assim Ujjwal <assimfrommohania@gmail.com>
2025-04-08 17:21:05 +0530

    [feature-43] Updated About Us Page

--- data/about.js
+++ data/about.js
@@ -1,79 +1,135 @@
-export const litigantsData = [
-  {
-    title: "Access from anywhere",
-    content:
-      "You can access the court from anywhere, 24x7. Respond to summons, submit documents, request to reschedule hearings, and appear for hearings from where you are.",
-  },
-  {
-    title: "Information on Finger-tips",
-    content:
-      "You will be able to login to the court system, and have a full and complete view of your case. You will receive proactive alerts and notifications on case updates and action to be taken such as documents to be filed, payments to be made. Receive verifiable copies of all your documents: applications, orders and judgments.",
-  },
-  {
-    title: "Reduced friction",
-    content:
-      "You will save time and effort on procedural matters such as adjournments, making payments, receiving copies of applications, orders and judgments. Your hearings will be more predictable and certain.",
-  },
-];
+export const headerData = {
+  title: "About Us",
+  description1: "The Kerala High Court is launching a new court, called 24x7 ON Courts, with a transformed dispute resolution experience for all users. The 24x7 ON Courts advance the vision outlined in Phase III of the Supreme Court eCourts policy. Starting with cheque-dishonor cases under the Negotiable Instruments Act that comprise 10% of the criminal pendency, the first of these courts, 24x7 ON Court Kollam will go live and accept the first case from November 2024.",
+  subTitle: "Vision",
+  description2: "24x7 ON Courts are like shifting from paper maps to digital maps. Just like a map app guides you to your destination, 24x7 ON Courts guide you towards justice. We envision a future in which courts go to people, instead of people going to courts. A future where courts proactively deliver and enable just-in-time services to people..."
+}
 
-export const advocatesData = [
-  {
-    title: "Predictable Schedule",
-    content:
-      "With the synchronized calendaring system, you can have greater predictability and visibility into your hearings. Virtually attend hearings and submit relevant documents and coordinate your schedule.",
-  },
-  {
-    title: "Intuitive processes",
-    content:
-      "Use the guidance from the digital system to reduce errors, make better applications and take timely action throughout the life-cycle of a case. Pay court fees and receive the certified true copies of your orders virtually.",
-  },
-  {
-    title: "No duplication",
-    content:
-      "Easily edit the digital case files to correct any errors. There is no need to submit or maintain a physical copy of the digital file.",
-  },
-];
+export const designPrinciplesData = {
+  title: "Design Principles",
+  subTitle: "ON Courts are guided by the following design principles:",
+  items: [
+    {
+      icon: "PeopleCentric",
+      title: "People Centric",
+      content:
+        "24x7 ON Courts transform processes to make court administration people centric. They prioritise understanding user needs to improve accessibility, fairness, equity, and empathy in the resolution process.",
+    },
+    {
+      icon: "Open",
+      title: "Open",
+      content:
+        "24x7 ON Courts make information open and actionable to the users, adopt open technology, access and data on functioning of courts. They are open subject to data governance frameworks to ensure privacy, security, and judicial integrity.",
+    },
+    {
+      icon: "DesignEngagement",
+      title: "Design for Engagement",
+      content:
+        "24x7 ON Courts are designed through an iterative process through consultations from various users of the court. They also enable integrations with allied departments and institutions to ensure seamless movement of information and updates. They will also open up APIs and enable the ecosystem to create needed services.",
+    },
+  ]
+};
 
-export const advocateClerksData = [
-  {
-    title: "Save time & effort",
-    content:
-      "Use your own unique login id into the system to take action: file case, submit documents, track case status and updates.",
-  },
-  {
-    title: "Information on Fingertips",
-    content:
-      "Easily access case updates and other information. You can access certified copies, orders, and judgments from anywhere. Reduced duplication of efforts by filing cases online.",
-  },
-];
+export const newExperienceData = {
+  title: "New Experience",
+  subTitle: "24x7 ON Courts will transform your experience in the following ways:",
+  items: [
+    {
+      title: "Accessible and Predictable",
+      content:
+        "Communicate with the court and take actions that progress your case asynchronously. File a case, respond to summons, submit applications and coordinate hearing schedules from anywhere at your convenience. Have greater predictability and visibility into your hearings.",
+    },
+    {
+      title: "Assisted and Empowering ",
+      content:
+        "Get real time updates on your case: amounts paid, next date of hearing, status of summons, and orders passed. Receive alerts for action to be taken including applications to be submitted or payments to be made. Use templates, prompts, dashboards to take action easily. Approach the courtroom manager for any assistance. ",
+    },
+    {
+      title: "Seamless & Frictionless ",
+      content:
+        "Interact with surrounding institutions such as banks, police, and post with ease. Track the real time status of summons. Proactively receive verifiable copies of all your documents: summons, orders and judgements.",
+    },
+  ]
+};
 
-export const judgesData = [
-  {
-    title: "Optimize judicial time",
-    content:
-      "You can optimize your judicial time through segregation of judicial and administrative functions. Through synchronised scheduling calendars, assistive templates and technology such as voice to text for easy transcription.",
-  },
-  {
-    title: "Data to assist Decision Making",
-    content:
-      "Interactive dashboards with case level information to make considered decisions.",
-  },
-  {
-    title: "Easy Access to Case files",
-    content:
-      "Access to files and documents submitted to know what to expect in each hearing and prepare accordingly.",
-  },
-];
-
-export const courtStaffData = [
-  {
-    title: "Reduced manual burden",
-    content:
-      "A natively digital system ensures no need for manual data entry. Rule-based case specific e-filing system minimises manual scrutiny efforts.",
-  },
-  {
-    title: "Assistive Tools",
-    content:
-      "You can use transcription tools for easy drafting of orders. OCR systems will also assist in scrutiny.",
-  },
-];
+export const benifitsData = {
+  title: "Benefits of 24x7 ON Court ",
+  subTitle: "ON Courts are designed to realize the vision of taking courts to people and offer multiple benefits:",
+  roles: {
+    litigants: [
+      {
+        title: "Access from anywhere",
+        content:
+          "You can access the court from anywhere, 24x7. Respond to summons, submit documents, request to reschedule hearings, and appear for hearings from where you are.",
+      },
+      {
+        title: "Information on Finger-tips",
+        content:
+          "You will be able to login to the court system, and have a full and complete view of your case. You will receive proactive alerts and notifications on case updates and action to be taken such as documents to be filed, payments to be made. Receive verifiable copies of all your documents: applications, orders and judgments.",
+      },
+      {
+        title: "Reduced friction",
+        content:
+          "You will save time and effort on procedural matters such as adjournments, making payments, receiving copies of applications, orders and judgments. Your hearings will be more predictable and certain.",
+      },
+    ],
+    advocates: [
+      {
+        title: "Predictable Schedule",
+        content:
+          "With the synchronized calendaring system, you can have greater predictability and visibility into your hearings. Virtually attend hearings and submit relevant documents and coordinate your schedule.",
+      },
+      {
+        title: "Intuitive processes",
+        content:
+          "Use the guidance from the digital system to reduce errors, make better applications and take timely action throughout the life-cycle of a case. Pay court fees and receive the certified true copies of your orders virtually.",
+      },
+      {
+        title: "No duplication",
+        content:
+          "Easily edit the digital case files to correct any errors. There is no need to submit or maintain a physical copy of the digital file.",
+      },
+    ],
+    advocateClerks: [
+      {
+        title: "Save time & effort",
+        content:
+          "Use your own unique login id into the system to take action: file case, submit documents, track case status and updates.",
+      },
+      {
+        title: "Information on Fingertips",
+        content:
+          "Easily access case updates and other information. You can access certified copies, orders, and judgments from anywhere. Reduced duplication of efforts by filing cases online.",
+      },
+    ],
+    judges: [
+      {
+        title: "Optimize judicial time",
+        content:
+          "You can optimize your judicial time through segregation of judicial and administrative functions. Through synchronised scheduling calendars, assistive templates and technology such as voice to text for easy transcription.",
+      },
+      {
+        title: "Data to assist Decision Making",
+        content:
+          "Interactive dashboards with case level information to make considered decisions.",
+      },
+      {
+        title: "Easy Access to Case files",
+        content:
+          "Access to files and documents submitted to know what to expect in each hearing and prepare accordingly.",
+      },
+    ],
+    courtStaffs: [
+      {
+        title: "Reduced manual burden",
+        content:
+          "A natively digital system ensures no need for manual data entry. Rule-based case specific e-filing system minimises manual scrutiny efforts.",
+      },
+      {
+        title: "Assistive Tools",
+        content:
+          "You can use transcription tools for easy drafting of orders. OCR systems will also assist in scrutiny.",
+      },
+    ]
+  }
+};
---
+++ pages/about/Benifits.tsx
@@ -0,0 +1,103 @@
+import Image from "next/image";
+import { benifitsData } from "../../data/about";
+import AccordionItem from "./AccordionItem";
+import { useState } from "react";
+
+export default function Benifits() {
+    const [openIndices, setOpenIndices] = useState({
+        litigants: null,
+        advocates: null,
+        advocateClerks: null,
+        judges: null,
+        courtStaffs: null,
+    });
+    const rolesMapping = {
+        litigants: "Litigants",
+        advocates: "Advocates",
+        advocateClerks: "Advocate Clerks",
+        judges: "Judges",
+        courtStaffs: "Court Staffs",
+    };
+
+    const handleToggle = (section, index) => {
+        setOpenIndices((prevState) => ({
+            ...prevState,
+            [section]: prevState[section] === index ? null : index,
+        }));
+    };
+
+    return <div className="bg-white rounded-lg shadow-lg mx-auto my-4 px-8 py-12 text-center text-[20px]">
+        <h2 className="text-[64px] font-bold text-teal text-center mb-2">
+            {benifitsData.title}
+        </h2>
+        <h4 className="text-[32px] text-center mb-8">
+            {benifitsData.subTitle}
+        </h4>
+        {Object.entries(benifitsData.roles).map(([role, data], index) => {
+            const isEven = index % 2 === 0;
+            return (
+                <div
+                    key={role}
+                    className={`flex mb-12 justify-between text-start items-start`}
+                >
+                    {/* Left Section */}
+                    {isEven ? (
+                        <>
+                            <div className="w-1/2 pr-8">
+                                <h3 className="text-[32px] font-bold text-teal mb-4 capitalize">{rolesMapping[role]}</h3>
+                                <div className="mb-8">
+                                    {data.map((item, i) => (
+                                        <AccordionItem
+                                            key={i}
+                                            title={item.title}
+                                            content={item.content}
+                                            isOpen={openIndices[role] === i}
+                                            onToggle={() => handleToggle(role, i)}
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+
+                            <div className="flex justify-center items-center w-1/2">
+                                <Image
+                                    src={`/images/roles/${role}.png`}
+                                    alt={`${role} Image`}
+                                    width={200}
+                                    height={100}
+                                    className="object-cover"
+                                />
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="flex justify-center items-center w-1/2">
+                                <Image
+                                    src={`/images/roles/${role}.png`}
+                                    alt={`${role} Image`}
+                                    width={200}
+                                    height={100}
+                                    className="object-cover"
+                                />
+                            </div>
+
+                            <div className="w-1/2 pr-8">
+                                <h3 className="text-[32px] font-bold text-teal mb-4 capitalize">{rolesMapping[role]}</h3>
+                                <div className="mb-8">
+                                    {data.map((item, i) => (
+                                        <AccordionItem
+                                            key={i}
+                                            title={item.title}
+                                            content={item.content}
+                                            isOpen={openIndices[role] === i}
+                                            onToggle={() => handleToggle(role, i)}
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                        </>
+                    )}
+                </div>
+            );
+        })}
+    </div>
+}
