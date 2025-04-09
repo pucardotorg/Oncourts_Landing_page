19baec41877dd3bf801559325153236069bff22b
ashutosh-beehyv <ashutosh.singh@beehyv.com>
2025-04-09 12:29:41 +0530

    #43 Add the WhatsNew component

---
+++ data/whatsNew.js
@@ -0,0 +1,44 @@
+export const liveNowFeatures = [
+    {
+      "S.No.": 1,
+      "Feature Description":
+        "Easier Bail Applications – Submit anytime, attach all documents, and include terms & conditions in one go.",
+      date: "04/11/2024",
+    },
+    {
+      "S.No.": 2,
+      "Feature Description":
+        "Clearly communicate what OnCourts is, its mission, and its future vision to the public and key stakeholders.",
+      date: "04/11/2024",
+    },
+  ];
+  
+  for (let i = 3; i <= 16; i++) {
+    liveNowFeatures.push({
+      "S.No.": i,
+      "Feature Description":
+        "Clearly communicate what OnCourts is, its mission, and its future vision to the public and key stakeholders.",
+      date: "04/11/2024",
+    });
+  }
+  
+  export const comingSoon = [
+    {
+      "S.No.": 1,
+      "Feature Description":
+        "Easier Bail Applications – Submit anytime, attach all documents, and include terms & conditions in one go. ",
+    },
+    {
+      "S.No.": 2,
+      "Feature Description":
+        "Clearly communicate what OnCourts is, its mission, and its future vision to the public and key stakeholders.",
+    },
+  ];
+  
+  for (let i = 3; i <= 16; i++) {
+    comingSoon.push({
+      "S.No.": i,
+      "Feature Description":
+        "Clearly communicate what OnCourts is, its mission, and its future vision to the public and key stakeholders.",
+    });
+  }
---
+++ pages/whats-new/index.tsx
@@ -0,0 +1,26 @@
+import FeaturesTable from "./FeaturesTable";
+import { liveNowFeatures, comingSoon } from "../../data/whatsNew";
+
+const WhatNew = () => {
+  return (
+    <div className="relative mx-auto mb-12 left-1/2 transform -translate-x-1/2 w-full max-w-[1858px] h-auto px-4 sm:px-8 gap-4 flex flex-col items-start">
+      <div className="w-full flex flex-col items-center gap-4">
+        <h1 className="text-[#007E7E] text-[36px] md:text-[48px] lg:text-[60px] font-bold leading-tight text-center mt-6 md:mt-12">
+          What’s New in 24x7 ON Courts
+        </h1>
+      </div>
+
+      <FeaturesTable
+        heading="Live Now: Latest Upgrades in ON Court"
+        data={liveNowFeatures}
+      />
+
+      <FeaturesTable
+        heading="Coming Soon: Upcoming Features in ON Court"
+        data={comingSoon}
+      />
+    </div>
+  );
+};
+
+export default WhatNew;
