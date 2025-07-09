3d4f3c2f47c065bad61f89d37bb8da4dabd5a7dc
ashishbeehyv <ashish.y@beehyv.com>
2025-07-10 02:47:23 +0530

    highlights section mobile view css update

--- components/HomePage/Highlights.tsx
+++ components/HomePage/Highlights.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useSafeTranslation } from "../../hooks/useSafeTranslation";
 import { svgIcons } from "../../data/svgIcons";
 import { transformImpactGlance } from "../../TransformData/transformResponseData";
+import { useMediaQuery } from "@mui/material";
 
 interface HighlightItemProps {
   value: string;
   label: string;
+  isMobile?: boolean;
 }
 
 export interface DashboardMetricsNew {
@@ -16,12 +18,18 @@ export interface DashboardMetricsNew {
   averageNumberOfDaysBetweenHearingsForCase: number;
 }
 
-const HighlightItem: React.FC<HighlightItemProps> = ({ value, label }) => (
-  <div className="flex flex-col items-center justify-center px-4 py-6 text-center">
+const HighlightItem: React.FC<HighlightItemProps> = ({
+  value,
+  label,
+  isMobile = false,
+}) => (
+  <div className="flex flex-col items-center justify-center px-4 text-center">
     <span className="font-noto font-bold text-[72.28px] h-[78.3px] leading-[78.3px] tracking-[-2.41px] text-center text-[#0F766E]">
       {value || "\u00A0"}
     </span>
-    <span className="h-[30px] font-roboto font-medium text-[26px] leading-[30px] tracking-[-0.26px] text-center text-[#0F172A]">
+    <span
+      className={`h-[30px] font-roboto font-medium text-[26px] leading-[30px] tracking-[-0.26px] text-center text-[#0F172A] mt-4 ${isMobile ? "text-[16px]" : "text-[26px]"}`}
+    >
       {label}
     </span>
   </div>
@@ -30,6 +38,7 @@ const HighlightItem: React.FC<HighlightItemProps> = ({ value, label }) => (
 const Highlights: React.FC = () => {
   const { t } = useSafeTranslation();
   const [stats, setStats] = useState<DashboardMetricsNew>();
+  const isMobile = useMediaQuery("(max-width: 640px)");
 
   useEffect(() => {
     const fetchImpactGlance = async () => {
@@ -60,30 +69,35 @@ const Highlights: React.FC = () => {
       value: stats?.averageNumberOfDaysBetweenHearingsForCase?.toString() || "",
       label: t("DAYS_TO_NEXT_HEARING"),
     },
-    {
-      value: stats?.daysToCaseRegistration?.toString() || "",
-      label: t("DAYS_TO_CASE_REGISTRATION"),
-    },
   ];
 
   return (
     <section className="w-full bg-white py-24">
       <div className="max-w-[1440px] mx-auto px-6">
-        <h2 className="font-libre h-[48px] font-[400] text-[40px] leading-[48px] tracking-normal text-center align-middle text-[#3A3A3A]">
-          {t("HIGHLIGHTS")}
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="flex items-center justify-center mb-8">
+          <h2 className="px-12 font-libre h-[70px] font-[400] text-[40px] leading-[48px] tracking-normal text-center align-middle text-[#3A3A3A] border-b border-[#CBD5E1]">
+            {t("HIGHLIGHTS")}
+          </h2>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {highlights.map((item, index) => (
-            <HighlightItem key={index} value={item.value} label={item.label} />
+            <HighlightItem
+              key={index}
+              value={item.value}
+              label={item.label}
+              isMobile={isMobile}
+            />
           ))}
         </div>
-        <div className="flex justify-center mt-8">
+        <div className="flex justify-center mt-12">
           <button
-            className="flex flex-row items-center justify-center px-4 md:px-[16px] gap-[12px] w-[376px] h-[69px] bg-white border border-[#0F766E] rounded-[12px] shadow-[inset_-2px_-2px_2px_rgba(15,23,42,0.14),inset_2px_2px_2px_1px_rgba(255,255,255,0.9)]"
+            className={`flex flex-row items-center justify-center px-4 md:px-[16px] gap-[12px] bg-white border border-[#0F766E] rounded-[12px] ${isMobile ? "h-[40px]" : "h-[69px]"}`}
             onClick={() => window.open("/", "_blank")}
           >
-            <svgIcons.OpenInNewTabIcon />
-            <span className="w-[296px] h-[32px] font-roboto font-medium text-[28px] leading-[32px] tracking-[-0.56px] text-center text-[#0F766E]">
+            <svgIcons.OpenInNewTabIcon width={isMobile ? "16" : "30"} />
+            <span
+              className={`h-[32px] font-roboto font-medium leading-[32px] tracking-[-0.56px] text-center text-[#0F766E] ${isMobile ? "text-[16px]" : "text-[28px]"}`}
+            >
               {t("VIEW_DETAILED_DASHBOARD")}
             </span>
           </button>
