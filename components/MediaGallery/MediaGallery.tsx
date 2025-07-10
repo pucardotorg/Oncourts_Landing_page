820d9011519d947f011a467eff683e4f16673873
ashishbeehyv <ashish.y@beehyv.com>
2025-07-10 22:25:26 +0530

    media gallery mobile responsive css adjusted

--- components/MediaGallery/MediaGallery.tsx
+++ components/MediaGallery/MediaGallery.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { galleryImages, GalleryImage } from "../../data/GalleryConfig";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+import { useMediaQuery } from "@mui/material";
 
 const MediaGallery: React.FC = () => {
   const [currentImage, setCurrentImage] = useState<GalleryImage>(
     galleryImages[0]
   );
+
+  const isMobile = useMediaQuery("(max-width: 640px)");
 
   const handlePrevious = () => {
     const currentIndex = galleryImages.findIndex(
@@ -33,15 +35,21 @@ const MediaGallery: React.FC = () => {
 
   return (
     <section className="pt-8 pb-24 bg-white">
-      <div className="w-[90vw] mx-auto px-4">
+      <div className={`w-[100vw] mx-auto px-4 ${isMobile ? "px-2" : "px-4"}`}>
         <div className="text-center mb-8">
-          <h1 className="text-[64px] leading-[70px] text-[#3A3A3A] font-libre mb-[20px] mx-[20px] border-b border-[#CBD5E1] pb-[10px]">
+          <h1
+            className={`text-[#3A3A3A] font-libre mb-[20px] border-b border-[#CBD5E1] pb-[10px] ${isMobile ? "text-[32px] leading-[40px] mx-[50px]" : "text-[40px] leading-[48px] mx-[60px]"}`}
+          >
             Media Gallery
           </h1>
-          <h2 className="text-[40px] leading-[48px]  font-libre font-normal text-center text-[#3A3A3A] mb-4 px-4">
+          <h2
+            className={` font-libre font-normal text-center text-[#3A3A3A] mb-4 px-4 ${isMobile ? "text-[24px] leading-[34px]" : "text-[40px] leading-[48px] "}`}
+          >
             Courtroom, ON Court Kollam
           </h2>
-          <p className="text-[28px] leading-[40px] font-roboto font-normal tracking-[-0.56px] text-center text-[#334155] px-4 mb-8">
+          <p
+            className={`font-roboto font-normal tracking-[-0.56px] text-center text-[#334155] px-4 mb-8 ${isMobile ? "text-[17px] leading-[22px]" : "text-[28px] leading-[40px]"}`}
+          >
             With a waiting area and help desk, the ON Court is designed to be
             people-centric. Equipped with state of art equipment, it enables
             quality virtual and hybrid hearing experience for users.
@@ -49,7 +57,9 @@ const MediaGallery: React.FC = () => {
         </div>
 
         {/* Main Image Display with Progress Bar */}
-        <div className="relative aspect-video w-full h-[70vh] mx-auto mb-8 px-4">
+        <div
+          className={`relative aspect-video w-full  mx-auto mb-8 px-4 ${isMobile ? "h-[170px]" : "h-[70vh]"}`}
+        >
           {/* Progress Bar */}
           <div className="absolute bottom-5 h-2 left-8 right-8 z-10">
             <div className="flex items-center gap-2">
@@ -67,7 +77,7 @@ const MediaGallery: React.FC = () => {
               src={currentImage.src}
               alt={currentImage.alt}
               fill
-              className="object-cover rounded-t-[12px]"
+              className={`object-cover ${isMobile ? "" : "rounded-t-[12px]"}`}
               priority
             />
           </div>
@@ -75,17 +85,17 @@ const MediaGallery: React.FC = () => {
           {/* Navigation Arrows */}
           <button
             onClick={handlePrevious}
-            className="absolute left-4 top-1/2 -translate-y-1/2 bg-white/80 hover:bg-white p-2 rounded-full shadow-lg transition-all"
+            className="absolute left-6 top-1/2 -translate-y-1/2 bg-[#00000033] hover:bg-white p-2 rounded-full shadow-lg transition-all"
             aria-label="Previous image"
           >
-            <ChevronLeftIcon className="w-6 h-6 text-gray-800" />
+            <ChevronLeftIcon className="w-6 h-6 text-[white] hover:text-[black]" />
           </button>
           <button
             onClick={handleNext}
-            className="absolute right-4 top-1/2 -translate-y-1/2 bg-white/80 hover:bg-white p-2 rounded-full shadow-lg transition-all"
+            className="absolute right-6 top-1/2 -translate-y-1/2 bg-[#00000033] hover:bg-white p-2 rounded-full shadow-lg transition-all"
             aria-label="Next image"
           >
-            <ChevronRightIcon className="w-6 h-6 text-gray-800" />
+            <ChevronRightIcon className="w-6 h-6 text-[white] hover:text-[black]" />
           </button>
         </div>
 
@@ -113,7 +123,7 @@ const MediaGallery: React.FC = () => {
               <button
                 key={image.id}
                 onClick={() => handleThumbnailClick(image)}
-                className={`relative aspect-video w-full overflow-hidden transition-all ${borderRadius} ${currentImage.id === image.id ? "ring-2 ring-[#3A3A3A]" : ""}`}
+                className={`relative aspect-video w-full overflow-hidden transition-all ${isMobile ? "" : borderRadius} ${currentImage.id === image.id ? "ring-2 ring-[#3A3A3A]" : ""}`}
               >
                 <Image
                   src={image.src}
