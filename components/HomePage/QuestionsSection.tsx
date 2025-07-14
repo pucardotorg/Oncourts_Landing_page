03b5821d67cf86462e10dc12c538057aafd61656
ashishbeehyv <ashish.y@beehyv.com>
2025-07-14 15:01:02 +0530

    FAQ icon position in parallel to first line of question

--- components/HomePage/QuestionsSection.tsx
+++ components/HomePage/QuestionsSection.tsx
@@ -22,18 +22,17 @@ const Question: React.FC<QuestionProps> = ({
   isMobile,
 }) => (
   <div className="border-b border-[#CBD5E1]">
-    <button
-      className="w-full flex items-center justify-between py-6 text-left"
-      onClick={onToggle}
-    >
-      <span
-        className={`font-sans font-medium  tracking-[0.01em] text-[#3A3A3A] pr-8 ${isMobile ? "text-[20px] leading-[24px]" : "text-[26px] leading-[36px]"}`}
-      >
-        {question}
-      </span>
-      <span className="flex-shrink-0">
-        {isOpen ? <svgIcons.UpArrowIcon /> : <svgIcons.DownArrowIcon />}
-      </span>
+    <button className="w-full py-6 text-left" onClick={onToggle}>
+      <div className="flex items-start justify-between">
+        <span
+          className={`font-sans font-medium tracking-[0.01em] text-[#3A3A3A] pr-8 ${isMobile ? "text-[20px] leading-[24px]" : "text-[26px] leading-[36px]"}`}
+        >
+          {question}
+        </span>
+        <span className={`flex-shrink-0  ${isMobile ? "" : "mt-[0.5em]"}`}>
+          {isOpen ? <svgIcons.UpArrowIcon /> : <svgIcons.DownArrowIcon />}
+        </span>
+      </div>
     </button>
     {isOpen && (
       <div className="pb-6">
