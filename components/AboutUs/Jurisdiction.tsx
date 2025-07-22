82feb52f5903f8c68407c14479e9223756238015
ashishbeehyv <ashish.y@beehyv.com>
2025-07-22 10:23:51 +0530

    enhancement- 4379- police stations map updated

--- components/AboutUs/Jurisdiction.tsx
+++ components/AboutUs/Jurisdiction.tsx
@@ -24,7 +24,7 @@ export default function Jurisdiction({ isMobile }: { isMobile: boolean }) {
             {/* Map image with markers */}
             <div className="relative">
               <Image
-                src="/images/policeStationMap.png"
+                src="/images/policeStationMapUpdated.png"
                 alt="Kollam District Map with Police Stations"
                 width={1200}
                 height={800}
@@ -35,7 +35,7 @@ export default function Jurisdiction({ isMobile }: { isMobile: boolean }) {
               {/* Fullscreen button */}
               <div className="absolute right-1 md:right-3 bottom-3 md:bottom-12">
                 <FullscreenButton
-                  url="/images/policeStationMap.png"
+                  url="/images/policeStationMapUpdated.png"
                   iconColor="#0F766E"
                   borderColor="#E2E8F0"
                   backgroundColor="white"
