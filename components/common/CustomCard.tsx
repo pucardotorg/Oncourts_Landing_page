79029543c03343dc1e584f25800ebc192d82e4cd
ashishbeehyv <ashish.y@beehyv.com>
2025-07-24 13:34:21 +0530

    4385 updated

--- components/common/CustomCard.tsx
+++ components/common/CustomCard.tsx
@@ -8,11 +8,13 @@ interface CustomCardProps {
 const CustomCard: React.FC<CustomCardProps> = ({ title, description }) => {
   return (
     <div className="font-roboto bg-white p-8 rounded-lg shadow-md flex flex-col">
-      <span className="text-[32px] font-medium font-roboto text-[#3A3A3A]">
+      <span className="text-[32px] leading-[36px] tracking-[-0.6px] font-medium font-roboto text-[#3A3A3A]">
         {title}
       </span>
       <div className="border-b border-[#CBD5E1] my-2 w-[80%]"></div>
-      <p className="text-[20px] font-normal text-[#334155]">{description}</p>
+      <p className="text-[20px] leading-[28px] tracking-[-0.2px] font-normal text-[#334155]">
+        {description}
+      </p>
     </div>
   );
 };
