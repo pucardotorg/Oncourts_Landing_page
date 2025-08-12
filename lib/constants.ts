8095fcb54da4386de813cef5f73f32be8f5cbc2b
Hemanth Reddy <156782186+hemanth-at-beehyv@users.noreply.github.com>
2025-08-12 14:50:02 +0530

    test

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
 export const GA_MEASUREMENT_ID = "G-6DRDK00D5W";
-export const GLOBAL_CONFIG_URL = "https://pucarfilestoreuat.blob.core.windows.net/pucar-filestore/pucar-assets/globalconfig/uat/globalconfig.js";
+export const GLOBAL_CONFIG_URL = "https://pucarfilestoreuat1.blob.core.windows.net/pucar-filestore/pucar-assets/globalconfig/uat/globalconfig.js";
