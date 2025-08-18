d6ddcc8ab95baff10809c5a3e7154d97913fab25
Assim Ujjwal <assimfrommohania@gmail.com>
2025-08-18 13:03:48 +0530

    Fixed localisation issue due to globalConfig

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,1 @@
 export const GA_MEASUREMENT_ID = "G-6DRDK00D5W";
-export const GLOBAL_CONFIG_URL = "https://pucarfilestoreuat.blob.core.windows.net/pucar-filestore/pucar-assets/globalconfig/uat/globalconfig.js";
