3428fa99b9a9f6290f881c4d166869bc1f22d657
rilsarojes26 <rilsarojes26@gmail.com>
2025-12-12 14:49:34 +0530

    [Bug] In landing page, font style is not as per figma for case search page buttons

--- components/CaseSearch/CNRForm.tsx
+++ components/CaseSearch/CNRForm.tsx
@@ -1,6 +1,6 @@
 const CNRForm = ({ caseNumber, setCaseNumber, config }) => (
   <div className="mb-4">
-    <label htmlFor="cnrInput" className="text-teal font-medium text-sm">
+    <label htmlFor="cnrInput" className="text-teal font-normal text-sm">
       {config.label}
     </label>
     <div className="rounded-xl border-2 border-teal p-2 mt-2">
